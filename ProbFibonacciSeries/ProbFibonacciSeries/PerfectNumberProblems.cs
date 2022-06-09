dd0fa180ba03a82d148ea7dae1bfccd6a015ccd1
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-06-10 02:53:24 +0530

    [Shivaraj] Added Stop watch problem solution

--- ProbFibonacciSeries/ProbFibonacciSeries/PerfectNumberProblems.cs
+++ ProbFibonacciSeries/ProbFibonacciSeries/PerfectNumberProblems.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//UC-2
+//a.Just like the Armstrong number, the Perfect Number is also a special type of
+//positive number. When the number is equal to the sum of its positive divisors
+//excluding the number, it is called a Perfect Number. For example, 28 is the perfect
+//number because when we sum the divisors of 28, it will result in the same number.
+//The divisors of 28 are 1, 2, 4, 7, and 14. So,
+//b. 28 = 1+2+4+7
+//c. 28 = 28
+
 namespace LogicalProblem
 {
     public class PerfectNumberProblems
--- ProbFibonacciSeries/ProbFibonacciSeries/ProbReverseNumber.cs
+++ ProbFibonacciSeries/ProbFibonacciSeries/ProbReverseNumber.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//UC-4
+//In C#, we can reverse a number either by using for loop, while loop, or using recursion.
+//The simplest way to reverse a number is by using for loop or while loop.In order to
+//reverse a number, we have to follow the following steps:
+//a.We need to calculate the remainder of the number using the modulo
+//b.After that, we need to multiply the variable reverse by 10 and add the remainder into it.
+//c. We then divide the number by 10 and repeat steps until the number becomes 0.
+
 namespace LogicalProblem
 {
     public class ReverseNumber
